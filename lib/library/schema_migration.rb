4d9eb8bd9ff0078b3c5db3401ffb86eb72c2fa84
Postmodern <postmodern.mod3@gmail.com>
2022-02-17 20:04:37 -0800

    Properly prefix the `schema_migration` table.

--- lib/library/schema_migration.rb
+++ lib/library/schema_migration.rb
@@ -3,7 +3,7 @@ require 'active_record'
 module Library
   class SchemaMigration < ActiveRecord::SchemaMigration
 
-    self.table_name = 'library_schema_migration'
+    self.table_name_prefix = 'library_'
 
   end
 end
