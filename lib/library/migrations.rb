4baeb38aca4597efbace6280cb3941113b31c16d
Postmodern <postmodern.mod3@gmail.com>
2025-05-26 13:38:41 -0700

    Updated `Library::Migrations` to not use the private ActiveRecord API.

--- lib/library/migrations.rb
+++ lib/library/migrations.rb
@@ -1,17 +1,24 @@
-require_relative 'migration_context'
+require 'active_record'
 
 module Library
   module Migrations
+    # The path to the `migrations/` directory.
+    DIR = File.join(__dir__,'migrations')
+
+    def self.context
+      @context ||= ActiveRecord::MigrationContext.new([DIR])
+    end
+
     def self.migrate(target_version=nil)
-      MigrationContext.new.migrate(target_version)
+      context.migrate(target_version)
     end
 
     def self.migrate_up(target_version=nil)
-      MigrationContext.new.up(target_version)
+      context.up(target_version)
     end
 
     def self.migrate_down(target_version=nil)
-      MigrationContext.new.down(target_version)
+      context.down(target_version)
     end
   end
 end
--- lib/library/schema_migration.rb
+++
@@ -1,9 +0,0 @@
-require 'active_record'
-
-module Library
-  class SchemaMigration < ActiveRecord::SchemaMigration
-
-    self.table_name_prefix = 'library_'
-
-  end
-end
